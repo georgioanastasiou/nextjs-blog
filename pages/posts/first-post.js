5de894f2645de3dbcf5b9c4b6686db67ed8492cc
George Anastasiou <g-anastasiou@hotmail.com>
2025-09-18 21:08:12 +0200

    First commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <>
+    <Head>
+        <title>First Post</title>
+    </Head>
+   
+    <Layout>
+      <h1>First Post</h1>
+      <h2>
+        <Link href={"/"}>Back to Home</Link>
+      </h2>
+    </Layout>
+    </>
+  );
+}
